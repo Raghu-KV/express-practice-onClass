13c7effb0106fa52e2652f35662fc46184b0f168
Raghu-KV <raghunandanv19@gmail.com>
2023-01-06 18:44:44 +0530

    _id

--- services/movies.service.js
+++ services/movies.service.js
@@ -1,6 +1,7 @@
 //in this file we write all the qeryes for the database
 
 import { client } from "../index.js";
+import { ObjectId } from "mongodb";
 
 export async function insertMovie(newData) {
   return await client.db("b40wd").collection("movies").insertMany(newData);
@@ -10,15 +11,21 @@ export async function updateMovieById(id, update) {
   return await client
     .db("b40wd")
     .collection("movies")
-    .updateOne({ _id: id }, { $set: update });
+    .updateOne({ _id: ObjectId(id) }, { $set: update });
 }
 
 export async function deleteMovieById(id) {
-  return await client.db("b40wd").collection("movies").deleteOne({ _id: id });
+  return await client
+    .db("b40wd")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
 
 export async function getMovieById(id) {
-  return await client.db("b40wd").collection("movies").findOne({ _id: id });
+  return await client
+    .db("b40wd")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 
 export async function getAllMovies(req) {
